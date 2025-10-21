aa8399f774d8cfc8c65911ccdd9dcfa67d92e046
Murat Bayraktar <bayraktar.muraat@gmail.com>
2025-10-21 14:09:59 +0300

    feat(raytracer): general structure outlined

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,15 +1,48 @@
 
 #include <iostream>
 #include "scene.h"
+#include "utils.h"
+#define OUTPUT_PATH "../my_outputs/"
+using namespace std;
+using namespace scene;
 
 
 int main(int argc, char* argv[])
 {
-    scene::Scene scene;
+    Scene scene;
     scene.loadSceneFromFile(argv[1]);
 
-    // Render the scene
     scene.getSummary();
 
+
+    vector<VectorFloatTriplet> meshNormals;
+    vector<VectorFloatTriplet> triangleNormals;
+
+    precomputeMeshNormals(scene.meshes, meshNormals, scene.vertices);
+    precomputeTriangleNormals(scene.triangles, triangleNormals, scene.vertices);
+
+    for (auto camera : scene.cameras) {
+        int width = camera.imageResolution.x;
+        int height = camera.imageResolution.y;
+        vector<unsigned char> image(width * height * 3);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                /*
+                    TODO: I have to do the following to compute pixelColor:
+                    1. Cast a ray from the camera to the pixel
+                    2. Check if the ray intersects with any object, I'll prolly need to write separate function for different objects
+                    3. If it intersects, compute the color of the pixel
+                    Somewhere around here I gotta apply shading
+                    4. If it doesn't intersect, return the background color
+                */
+                VectorFloatTriplet pixelColor = {0, 0, 0};
+                image[y * width + x * 3] = pixelColor.x;
+                image[y * width + x * 3 + 1] = pixelColor.y;
+                image[y * width + x * 3 + 2] = pixelColor.z;
+            }
+        }
+        writePPM((OUTPUT_PATH + camera.imageName).c_str(), image, width, height);
+    }
     return 0;
 }
+ 
